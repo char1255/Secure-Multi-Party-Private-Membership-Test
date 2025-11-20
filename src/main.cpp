c78fcbeae6f85dd633f4b0326cb4a3b58481c3da
penelope1255 <artemis03668@gmail.com>
2025-11-21 05:56:17 +0800

    Min-2025-11-21-05-56

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,12 @@
-// #include <iostream>
-// #include <chrono>
-// #include <xsimd/xsimd.hpp>
+#include <iostream>
+#include <chrono>
+#include <xsimd/xsimd.hpp>
 
-// int main(int argc, char** argv)
-// {
-//     (void)argc; (void)argv;
-//     // std::cout << "Secure Multi-Party Private Membership Test\n";
+int main(int argc, char** argv)
+{
+    (void)argc; (void)argv;
+    // std::cout << "Secure Multi-Party Private Membership Test\n";
 
     
-//     return 0;
-// }
-
-
-#include <stdint.h>
-#include <stdio.h>
-
-int main() {
-    uint32_t x = 0x01020304;
-    uint8_t *p = (uint8_t*)&x;
-
-    if (p[0] == 0x04)
-        printf("small\n");
-    else if (p[0] == 0x01)
-        printf("big\n");
-    else
-        printf("strange\n");
+    return 0;
 }
